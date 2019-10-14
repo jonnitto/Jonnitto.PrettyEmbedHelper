f000e93be8b5b5945ee6db87a4cdd4354dbb204c
Jon Uhlmann <me@jonnitto.ch>
2019-10-14 23:15:31 +0200

    :bug: Fix IE issue

--- Resources/Private/Assets/Helper/addEvent.js
+++ Resources/Private/Assets/Helper/addEvent.js
@@ -1,9 +1,32 @@
+function matches(element, selector) {
+    return (
+        element.matches ||
+        element.matchesSelector ||
+        element.msMatchesSelector ||
+        element.mozMatchesSelector ||
+        element.webkitMatchesSelector ||
+        element.oMatchesSelector
+    ).call(element, selector);
+}
+
+function closest(element, selector) {
+    let ancestor = element;
+
+    do {
+        if (matches(ancestor, selector)) {
+            return ancestor;
+        }
+        ancestor = ancestor.parentElement || ancestor.parentNode;
+    } while (ancestor !== null && ancestor.nodeType === 1);
+    return null;
+}
+
 export default function(selector, callback) {
     document.documentElement.addEventListener('click', event => {
-        let element = event.target.closest(selector);
-        if (!element || typeof callback != 'function') {
+        const ELEMENT = closest(event.target, selector);
+        if (!ELEMENT || typeof callback != 'function') {
             return;
         }
-        callback.call(element, event);
+        callback.call(ELEMENT, event);
     });
 }
