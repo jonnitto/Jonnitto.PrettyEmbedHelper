dd6b116c4267e2b383f982029a35c439ab27aefe
Jon Uhlmann <me@jonnitto.ch>
2020-03-06 20:58:43 +0100

    :recycle: Remove unused import

--- Resources/Private/Assets/Audio/Embed.js
+++ Resources/Private/Assets/Audio/Embed.js
@@ -1,9 +1,6 @@
 import { init } from '../Helper/MediaInit';
-import addEvent from '../Helper/addEvent';
 
-const elements = document.querySelectorAll(
-    '.jonnitto-prettyembed--audio audio'
-);
+const elements = document.querySelectorAll('.jonnitto-prettyembed--audio audio');
 Array.from(elements).forEach(element => {
     init(element, false);
 });
