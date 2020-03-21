4327b14f6016f927853726ad9aca2ac6a4644cb6
Jon Uhlmann <me@jonnitto.ch>
2020-03-21 23:06:03 +0100

    :arrow_up: Upgrading dependencies and also remove rollup-plugin-license

--- Resources/Public/Scripts/Backend.js
+++ Resources/Public/Scripts/Backend.js
@@ -4,6 +4,5 @@
  * Copyright 2019-2020 Jon Uhlmann
  * Licensed under GPL-3.0-or-later
  */
-
 !function(){"use strict";function t(t){!function(){var t=!(arguments.length>0&&void 0!==arguments[0])||arguments[0],e=arguments.length>1?arguments[1]:void 0,n=!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(window.navigator.userAgent);!!(!n&&!t||n&&t)&&("function"==typeof e&&e())}(!0,t)}function e(t){var n=t.getAttribute("src");t.naturalHeight<=90&&t.naturalWidth<=120&&-1==n.indexOf("/default.jpg")&&(n=n.replace("mqdefault","default").replace("hqdefault","mqdefault").replace("sddefault","hqdefault").replace("maxresdefault","sddefault"),t.setAttribute("src",n),setTimeout((function(){t.onload=function(){e(t)}}),10),setTimeout((function(){e(t)}),5e3))}window.addEventListener("load",(function(){var n;t((function(){void 0===n&&(n=document.querySelectorAll("img.jonnitto-prettyembed__youtube-preview"));for(var t=n.length-1;t>=0;t--)e(n[t])}))}))}();
 //# sourceMappingURL=Backend.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,21 @@
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import license from 'rollup-plugin-license';
 import composer from './composer.json';
 
 const AUTHOR = composer.authors[0].name;
-const BANNER_CONTENT = `${composer.extra.neos['package-key']} - created by ${AUTHOR}
-@link ${composer.homepage}
-Copyright 2019-${parseInt(new Date().getFullYear(), 10)} ${AUTHOR}
-Licensed under ${composer.license}`;
+const BANNER_CONTENT = `/*!
+ * ${composer.extra.neos['package-key']} - created by ${AUTHOR}
+ * @link ${composer.homepage}
+ * Copyright 2019-${parseInt(new Date().getFullYear(), 10)} ${AUTHOR}
+ * Licensed under ${composer.license}
+ */`;
 
 export default [
     {
         input: 'Resources/Private/Assets/Main.js',
-        plugins: [
-            babel(),
-            terser({
-                output: {
-                    comments: false
-                }
-            }),
-            license({
-                banner: {
-                    content: BANNER_CONTENT,
-                    commentStyle: 'ignored'
-                }
-            })
-        ],
+        plugins: [babel(), terser()],
         output: {
+            banner: BANNER_CONTENT,
             sourcemap: true,
             file: 'Resources/Public/Scripts/Main.js',
             format: 'iife'
@@ -34,21 +23,9 @@ export default [
     },
     {
         input: 'Resources/Private/Assets/Backend.js',
-        plugins: [
-            babel(),
-            terser({
-                output: {
-                    comments: false
-                }
-            }),
-            license({
-                banner: {
-                    content: BANNER_CONTENT,
-                    commentStyle: 'ignored'
-                }
-            })
-        ],
+        plugins: [babel(), terser()],
         output: {
+            banner: BANNER_CONTENT,
             sourcemap: true,
             file: 'Resources/Public/Scripts/Backend.js',
             format: 'iife'
