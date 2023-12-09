9313f4742ad32afcc0fc0d8f4cce564b45489538
Jon Uhlmann <account@uhlmann.pro>
2023-12-09 23:44:54 +0100

    Fix: Make Player work in Backend

---
+++ Resources/Private/Assets/Backend.js
@@ -0,0 +1,11 @@
+// Disable clicks in backend
+
+let firstRun = true;
+
+if (firstRun && window.name == 'neos-content-main') {
+    firstRun = false;
+    const element = document.createElement('style');
+    element.setAttribute('rel', 'stylesheet');
+    element.innerText = '.jonnitto-prettyembed-button{pointer-events:none !important}'
+    document.head.appendChild(element);
+}
