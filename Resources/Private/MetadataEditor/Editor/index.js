d418a0cf17e2cf2c4c38389a4cec18b7d0300acd
Jon Uhlmann <account@uhlmann.pro>
2023-12-11 02:16:32 +0100

    Update: Add own CSS Module

---
+++ Resources/Private/MetadataEditor/Editor/index.js
@@ -0,0 +1,57 @@
+import React, { useState, Fragment } from 'react';
+import { neos } from '@neos-project/neos-ui-decorators';
+import style from './style.module.css';
+import clsx from 'clsx';
+
+const neosifier = neos((globalRegistry) => ({
+    i18nRegistry: globalRegistry.get('i18n'),
+}));
+
+function Editor(props) {
+    const { value, options, i18nRegistry } = props;
+
+    const array = Object.entries(options)
+        .map(([key, labelKey]) => {
+            const itemValue = value[key];
+            if (!itemValue) {
+                return null;
+            }
+            const secondValue = key === 'duration' ? convertSeconds(itemValue) : null;
+            return {
+                label: i18nRegistry.translate(labelKey + '.label'),
+                value: i18nRegistry.translate(labelKey + '.value', itemValue, [itemValue, secondValue]),
+            };
+        })
+        .filter((value) => !!value);
+
+    if (!array.length) {
+        return <div>{i18nRegistry.translate('Jonnitto.PrettyEmbedHelper:NodeTypes.Mixin.Metadata:noMetadataSet')}</div>;
+    }
+
+    return (
+        <dl className={style.infoView}>
+            {array.map(({ label, value }) => (
+                <Fragment>
+                    <dt className={style.propertyLabel}>{label}</dt>
+                    <dd className={style.propertyValue} dangerouslySetInnerHTML={{ __html: value }}></dd>
+                </Fragment>
+            ))}
+        </dl>
+    );
+}
+
+const convertSeconds = (duration) => {
+    const hours = ~~(duration / 3600);
+    const minutes = ~~((duration % 3600) / 60);
+    const seconds = duration % 60;
+
+    if (hours) {
+        return `${hours}:${minutes}:${seconds}`;
+    }
+    if (minutes) {
+        return `${minutes}:${seconds}`;
+    }
+    return '';
+};
+
+export default neosifier(Editor);
--- build.mjs
+++ build.mjs
@@ -1,4 +1,6 @@
 import esbuild from 'esbuild';
+import extensibilityMap from '@neos-project/neos-ui-extensibility/extensibilityMap.json' assert { type: 'json' };
+import { cssModules } from 'esbuild-plugin-lightningcss-modules';
 
 const baseOptions = {
     logLevel: 'info',
@@ -25,15 +27,34 @@ const moduleOptions = {
     splitting: true,
 };
 
+const editorOptions = {
+    ...baseOptions,
+    entryPoints: { Metadata: 'Resources/Private/MetadataEditor/manifest.js' },
+    outdir: 'Resources/Public/Plugin',
+    format: 'iife',
+    loader: {
+        '.js': 'tsx',
+        '.pcss': 'css',
+    },
+    alias: extensibilityMap,
+    plugins: [
+        cssModules({
+            targets: {
+                chrome: 80, // aligns somewhat to es2020
+            },
+            cssModules: {
+                dashedIdents: true,
+                pattern: 'jonnitto-prettyembed-[hash]-[local]',
+            },
+        }),
+    ],
+};
+
 async function watch(options) {
     const context = await esbuild.context(options);
     await context.watch();
 }
 
-if (process.argv.includes('--watch')) {
-    watch(scriptOptions);
-    watch(moduleOptions);
-} else {
-    esbuild.build(scriptOptions);
-    esbuild.build(moduleOptions);
-}
+[scriptOptions, moduleOptions, editorOptions].forEach((options) => {
+    process.argv.includes('--watch') ? watch(options) : esbuild.build(moduleOptions);
+});
