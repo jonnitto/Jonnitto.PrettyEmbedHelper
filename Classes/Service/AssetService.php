2b33c5b0ceda7413464526bbe0513ad37daeaa5b
Jon Uhlmann <account@uhlmann.pro>
2025-12-16 13:05:40 +0100

    Update: Save more metadata in audio files

--- Classes/Service/AssetService.php
+++ Classes/Service/AssetService.php
@@ -11,8 +11,9 @@ use Neos\Flow\Utility\Environment;
 use Neos\Flow\Utility\Exception;
 use Neos\Utility\Exception\FilesException;
 use Neos\Utility\Files;
+use DateTime;
 use function file_exists;
-use function round;
+use function floor;
 
 #[Flow\Scope('singleton')]
 class AssetService
@@ -56,6 +57,7 @@ class AssetService
     public function getAndSaveDataId3(NodeInterface $node, bool $remove, string $type): array
     {
         $duration = null;
+        $audio = null;
 
         if ($remove === true || !class_exists('JamesHeinrich\GetID3\GetID3')) {
             Utility::removeMetadata($node, 'duration');
@@ -68,10 +70,18 @@ class AssetService
                 $file = $assets[0]->getResource()->createTemporaryLocalCopy();
                 if (file_exists($file)) {
                     $fileInfo = $getID3->analyze($file);
-                    $duration = (int) round($fileInfo['playtime_seconds']);
+                    $duration = (int) floor($fileInfo['playtime_seconds']);
+
+                    if ($type === 'Audio') {
+                        $audio = $this->audioData($fileInfo, $duration);
+                    }
                 }
             }
-            Utility::setMetadata($node, 'duration', $duration);
+            if ($type === 'Audio') {
+                Utility::setMetadata($node, null, $audio);
+            } else {
+                Utility::setMetadata($node, 'duration', $duration);
+            }
         }
 
         return [
@@ -82,5 +92,41 @@ class AssetService
             'path' => $node->getPath(),
             'data' => isset($duration),
         ];
+    }
+
+    private function audioData(array $fileInfo, int $duration): array
+    {
+        if (isset($fileInfo['tags'])) {
+            $idKey = array_key_first($fileInfo['tags']);
+            $tags = $fileInfo['tags'][$idKey];
+        }
+
+        if (!isset($tags) && isset($fileInfo['id3v2']) && isset($fileInfo['id3v2']['comments'])) {
+            $tags = $fileInfo['id3v2']['comments'];
+        }
+
+        if (!isset($tags) && isset($fileInfo['id3v1'])) {
+            $tags = $fileInfo['id3v1'];
+        }
+
+        if (isset($tags)) {
+            $year = $tags['year'][0] ?? null;
+            $monthAndDay = $tags['date'][0] ?? null;
+
+            if ($year && $monthAndDay) {
+                $date = DateTime::createFromFormat('Ydm-H:i', $year . $monthAndDay . '-00:00');
+            }
+            $title = $tags['title'][0] ?? null;
+            $artist = $tags['artist'][0] ?? null;
+            $album = $tags['album'][0] ?? null;
+        }
+
+        return array_filter([
+            'duration' => $duration,
+            'date' => $date ?? null,
+            'title' => $title ?? null,
+            'artist' => $artist ?? null,
+            'album' => $album ?? null,
+        ]);
     }
 }
